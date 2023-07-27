cb2933ba85c674f8275dab6840bbab55095a2066
Dan Willman <github@danwillman.com>
2023-07-27 14:54:04 -0500

    initial commit

---
+++ ManageMint/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ManageMint;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ ManageMint/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ManageMint;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
