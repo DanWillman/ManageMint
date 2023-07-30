71e65fbaad4a38c513ffe02ad0fc3b44253e7519
Dan Willman <github@danwillman.com>
2023-07-30 10:28:28 -0500

    add unit test proj

---
+++ tests/ManageMint.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ManageMint.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
